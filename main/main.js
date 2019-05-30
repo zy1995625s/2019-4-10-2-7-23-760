ec5e07cca92328697329702428633c0d324474b5
zy1995625s <zy1995625s@gmail.com>
2019-05-30 15:48:23 +0800

    出租车计价

--- main/main.js
+++ main/main.js
@@ -1,4 +1,19 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    const startPrice = 6;
+    const pricePerKM = 0.8;
+    const parkTimePrice = 0.25;
+    const Over8KMPercent = 0.5;
+    var distance = inputs["distance"];
+    var parkTime = inputs["parkTime"];
+    var price = 0;
+    if(distance <= 2) {
+        price += startPrice;
+    } else {
+        price += startPrice + pricePerKM * (distance - 2);
+        if(distance >= 8) {
+            price += pricePerKM * Over8KMPercent * (distance - 8);
+        }
+    }
+    price += parkTime * parkTimePrice;
+    return Math.round(price);
 };
